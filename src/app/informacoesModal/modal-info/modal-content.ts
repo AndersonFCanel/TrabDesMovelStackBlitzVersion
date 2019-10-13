6df5db7d417e86a8686e8ee2500bf15289f5900d
AndersonFCanel <andersonfcanel@gmail.com>
2019-10-13 00:44:55 -0300

    1 file modified, 1 added

--- src/app/informacoesModal/modal-info/modal-content.ts
+++ src/app/informacoesModal/modal-info/modal-content.ts
@@ -4,31 +4,7 @@ import { ValidacaoService } from '../../validacao.service';
 
 @Component({
   selector: 'ngbd-modal-content',
-  template: `
-    <div class="modal-header">
-      <h4 class="modal-title">Outra Partida ou Reiniciar Rodada?</h4>
-      <button type="button" class="close" aria-label="Close" (click)="activeModal.dismiss('Cross click')">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>
-    <div class="modal-body">
-      
-    </div>
-    <div class="modal-footer">
-      <div class="row">
-        <div class="col-5 col-lg-5 ">
-            <button (click)="restart(false)" type="button" class="btn btn-warning btn-lg btn-block">Limpar
-                Tabuleiro</button>
-        </div>
-        <div class="col-2 col-lg-2 ">
-        </div>
-        <div class="col-5 -lg-5 ">
-            <button (click)="restart(true)" type="button" class="btn btn-danger btn-lg btn-block">Reiniciar
-                Partida</button>
-        </div>
-    </div>
-    </div>
-  `
+  templateUrl:'./modal-content.ht'
 })
 export class NgbdModalContent {
  // @Input() name ;
