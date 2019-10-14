8b0c68b8f0cd427c292aa040de06c91846825c71
AndersonFcanel <andersonfcanel@gmail.com>
2019-10-13 23:39:52 -0300

    +

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Trabalho A1 - Desenvolvimento Móvel';
+  title = 'Desenvolvimento Móvel - Jogo da Velha';
 }
