5c088c1172877d2f8de356bdc6a08db8fbf74f2e
AndersonFCanel <andersonfcanel@gmail.com>
2019-10-14 10:14:26 -0300

    1 file modified

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,23 +13,5 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'NG-CALCULADORA'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('NG-CALCULADORA');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to NG-CALCULADORA!');
-  });
+  
 });
